71aeaa99385851000e4360527abbd2dd8745a5a5
HaleemaNaeem <haleemanaeem53@gmail.com>
2024-08-25 07:15:29 -0700

    commit 1

---
+++ main.py
@@ -0,0 +1,43 @@
+## write a program that calculates the volume of cylinder using formula.
+r:int=int(input("enter the radius of cylinder"))
+h:int=int(input("enter the height of cylinder"))
+v:int=3.14*(r*r)*h
+print(f"the volume of cylinder is{v}")
+##write aprogram that calculates the BMI using height in (meters)and weight in(kilograms)variables
+Kg:int=int(input("enter the weight of individual"))
+m:int=int(input("enter the height of individual"))
+BMI:int=kg//(m*m)
+print(f"the BMI Of the individual is{BMI}")
+## write a program that calculates the percentage.
+value:int=int(input("enter the value"))
+percentage:int=int(input("enter the percentage value"))
+print (f" the percentage value is{(value/100)*percentage}")
+## convert a given number of seconds into minutes and seconds using variables.
+s:int=int(input("enter the no of seconds"))
+minutes:int=s//60
+s:int=m%60
+print (f"the no of seconds{s} and minutes {m}")
+##create a program that converta a teprature from farhenheit to celsius and vice versa using variablr
+temp:int=int(input("enter temp in celsius"))
+temprature:int=((9/5*temp)+32)
+print(f"the tempratureis farhenheit is{temprature}")
+Temprature:int(input("enter temp in farhenheit"))
+temp:int=((32*temp-32)*5/9)
+print(f"the t in celsius is {temp}")
+##write a program that calculates the area of cube
+a:int=int(input("enter the side of cube"))
+Area:int=(6*(a*a))
+print (f"the area of cube is {Area}")
+##write a program that calculates the area of circle
+a:int=int(inpur("enter the radius of circle"))
+AREA:int=(3.14*(r*r))
+print (f"the area of circle {Area}")
+##write a program that calculates the area of rectangle using length and width variable
+l:int=int(input("enter the length of rectangle)"))
+w :int=int(input("enter the width of rectangle)"))
+area:int=w*l
+print(f"the area of rectangle{area})")
+##calculate tha age based on current year and yoour birth year
+birth yrear:int=int(input("ENter your birth year))"))
+current year:int=int(input("enter your current year"))
+print(f"your age is{current year -birth year}")
